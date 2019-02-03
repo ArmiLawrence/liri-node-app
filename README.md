7555f7c4ab6c9d67b5b0addba7790e524d87a07f
Armi <armi.avelino@gmail.com>
2019-02-03 14:39:35 -0700

    First Push

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # liri-node-app
+
+LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+1. LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+2. Node packages downloaded for use in this app:
+    * [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+    * [Axios](https://www.npmjs.com/package/axios)
+    * [Moment](https://www.npmjs.com/package/moment)
+    * [DotEnv](https://www.npmjs.com/package/dotenv)
+3. Instructions on how to use LIRI
+    * Open Git Bash or Terminal
+    * Navigate to the folder with LIRI and its components
+    * Check and make sure the liri.js file exists
+    * Check and make sure package.json exists/node_modules folder
+        * If node_modules folder does not exist, run npm install in Git Bash/Terminal and node_modules will be downloaded
+        * If package.json does not exist, let me know and that means I missed a big part of the assignment :)
+    * cd into the folder with LIRI: 
+    * To get concert info, type: node liri.js concert-this artist name
+        * See screenshot in Images folder - Images/Concerts.PNG
+    * To get music info, type: node liri.js spotify-this-song song name
+        * See screenshot in Images folder - Images/Spotify.PNG
+    * To get movie info, type: node liri.js movie-this movie name
+        * See screenshot in Images folder - Images/Movie.PNG
+    * To use a random text file instead of typing in Git Bash/Terminal, add the commands concert-this, spotify-this, movie-this and the respective names of artists, songs, movies in the Random.txt file, then type: node liri.js do-what-it-says
+4. Tools used in building LIRI
+    * Node.js
+        * See Node packages downloaded above
+    * Javascript
+    * Git Bash/Terminal
---
+++ liri.js
@@ -0,0 +1,216 @@
+require("dotenv").config();
+
+// Grab the packages...
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var moment = require("moment");
+
+//inputs
+var keys = require("./keys");
+var spotify = new Spotify (keys.spotify);
+var action = process.argv[2];
+var nodeArgs = process.argv;
+
+// Functions
+
+    // bands in town/ concerts-this
+
+    function bandsInTown(){
+
+        var artist = "";
+
+        // Loop through all the words in the node argument
+        // And do a little for-loop magic to handle the inclusion of "+"s
+        for (var i = 3; i < nodeArgs.length; i++) {
+
+            if (i > 3 && i < nodeArgs.length) {
+            artist = artist + "+" + nodeArgs[i];
+            }
+            else {
+            artist += nodeArgs[i];
+        
+            }
+        }
+
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+        // Run the axios.get function...
+        // The axios.get function takes in a URL and returns a promise (just like $.ajax)
+        axios
+        .get(queryUrl)
+        .then(function(response) {
+            // If the axios was successful...Then log the body from the site!
+
+            // picking 5 concerts for fun//
+            for (i = 0; i < 5; i++) {
+
+            var venue = response.data[i].venue.name;
+            var location = response.data[i].venue.city;
+            var date = response.data[i].datetime;
+
+            console.log("Venue Name: " + venue);
+            console.log("Venue City: " + location);
+            //using moment to capture format of dates
+            console.log("Venue Date: " + moment(date).format("MM/DD/YYYY"));
+
+
+            }
+        });
+    };
+
+    // spotify - songs - spotify-this-song
+
+    function spotifySong() {
+        // empty variable to hold the song;
+        var song = "";
+
+        // Loop through all the words in the node argument
+        // And do a little for-loop magic to handle the inclusion of "+"s
+        for (var i = 3; i < nodeArgs.length; i++) {
+
+            if (i > 3 && i < nodeArgs.length) {
+            song = song + "+" + nodeArgs[i];
+            }
+            else {
+            song += nodeArgs[i];
+        
+            }
+        }
+
+        //If no song is provided then your program will default to "The Sign" by Ace of Base.
+        if (song === ""){
+            song = "The Sign"
+        };
+
+        //limiting 5 songs (randomly)
+        spotify.search({ type: 'track', query: song, limit: 5}, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+          
+            //looping to 5 to match with limit up top//
+            for (i = 0; i < 5; i++) {
+
+            var songName = data.tracks.items[i].name;
+            var album = data.tracks.items[i].album.name;
+            var artist = data.tracks.items[i].artists[0].name;
+            var preview = data.tracks.items[i].preview_url;
+        
+            console.log("Song Name: " + songName);
+            console.log("Album Name: " + album);  
+            console.log("Artist : " + artist); 
+            console.log("Preview Link: " + preview);
+            
+            
+            }
+          });
+        
+        };
+
+    // movies - OMDB -movie-this
+
+    function movie(){
+            
+        // Create an empty variable for holding the movie name
+        var movieName = "";
+           
+        // Loop through all the words in the node argument
+        // And do a little for-loop magic to handle the inclusion of "+"s
+        for (var i = 3; i < nodeArgs.length; i++) {
+
+            if (i > 3 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
+            }
+            else {
+                movieName += nodeArgs[i];
+            }
+        };
+
+        //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+        if (movieName === ""){
+            movieName = "Mr. Nobody"
+        }; 
+
+        // Then run a request with axios to the OMDB API with the movie specified
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+        // This line is just to help us debug against the actual URL.
+
+        axios.get(queryUrl).then(
+            function(response) {
+                console.log("Title:" + response.data.Title);
+                console.log("Release Year: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("Production Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+
+            }
+        );
+    };     
+
+    //reading from the random.txt - do what it says
+
+    function doIt() {
+
+        fs.readFile("random.txt", "utf8", function(error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+               
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+        console.log(dataArr); //looking at the array
+        console.log(dataArr[0]); //first thing in the array -- the command of what we're doing
+        console.log(dataArr[1].slice(1, -1)); //the second thing in the array/removing the "" so text can be read.
+        
+        //These do not work, the functions cannot read the words passed through the data array
+        
+            if (dataArr[0] === "spotify-this-song") {
+                console.log("spotify!")
+                var song = dataArr[1].slice(1, -1);
+                spotifySong(song); //function cannot read the song variable and is sending an error             
+            } else if (dataArr[0] === "concert-this") {
+                console.log("bands in town!")
+                //var artist = dataArr[1].slice(1, -1);
+                //bandsInTown(artist);                
+            } else if(dataArr[0] === "movie-this") {
+                console.log("movie this!")
+                var movieName = dataArr[1].slice(1, -1);                                             
+                console.log(movieName); //console can read the movie name
+                movie(movieName); //spits out Mr. Nobody
+            }
+        });
+    }
+
+//lets do stuff
+
+if (action === "concert-this") {
+
+    bandsInTown();
+
+    } 
+
+    else if (action === "spotify-this-song")  {
+
+    spotifySong();  
+
+    }
+    
+    else if (action === "movie-this"){
+
+    movie();   
+    
+    }
+        
+    else if (action === "do-what-it-says") {
+
+    doIt();
+    
+    };
+
